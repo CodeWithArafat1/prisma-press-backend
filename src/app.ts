e21532e8407f5b187b91538fdb21c3724b99962e
Arafat Nill <arafat-nill@Nill.netis.cc>
2026-07-01 08:37:30 +0600

    try agin deoploy

--- src/app.ts
+++ src/app.ts
@@ -7,22 +7,25 @@ import cookieParser from "cookie-parser";
 import { postRoutes } from "./modules/post/post.routes";
 import { commentRoutes } from "./modules/comment/comment.routes";
 
-
 const app = express();
 app.use(
   cors({
-    origin: config.app_url, // Allow requests from the specified app URL
+    origin: [
+      config.app_url as string,
+      "http://localhost:3000",
+      "http://localhost:5173",
+    ].filter(Boolean),
     credentials: true,
   }),
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser())
+app.use(cookieParser());
 
 // create a new user
 app.use("/api/users", userRouter);
-app.use('/api/auth', authRoutes)
-app.use('/api/posts', postRoutes)
-app.use('/api/comments', commentRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
 
 export default app;
